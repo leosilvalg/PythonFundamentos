6a23afa9c4aecd4ee96657a5acac5d2130aafe5c
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-02-12 10:19:36 -0300

    Criação de uma calculadora básica em python. Lab02

--- Cap03/Lab02/calculadora_v1.py
+++ Cap03/Lab02/calculadora_v1.py
@@ -7,3 +7,51 @@
 print("\n******************** Python Calculator ********************")
 
 
+print('Selecione a operação desejada: \n')
+
+def soma(x, y):
+    print("Soma: ", x + y)
+
+def sub(x, y):
+    print('Subtração: ', x - y)
+
+def mult(x, y):
+    print('Multiplicação: ', x * y)
+
+def div(x, y):
+    print('Divisão: ', x / y)
+
+
+print('1 - Soma')
+print('2 - Subtração')
+print('3 - Multiplicação')
+print('4 - Divisão \n')
+
+info = input('Digite a operação desejada (1/2/3/4): \n')
+    
+if int(info) == 1:
+    num1 = input('Digite o primeiro valor \n')
+    num2 = input('Digite o segundo valor \n')
+
+    soma(int(num1), int(num2))
+
+elif int(info) == 2:
+    num1 = input('Digite o primeiro valor \n')
+    num2 = input('Digite o segundo valor \n')
+
+    sub(int(num1), int(num2))
+
+elif int(info) == 3:
+    num1 = input('Digite o primeiro valor \n')
+    num2 = input('Digite o segundo valor \n')
+
+    mult(int(num1), int(num2))
+
+elif int(info) == 4:
+    num1 = input('Digite o primeiro valor \n')
+    num2 = input('Digite o segundo valor \n')
+
+    div(int(num1), int(num2))
+
+else:
+    print('Operação inválida!')
